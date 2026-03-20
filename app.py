c1e059355a8d7d7b633726b26e96297097d24f27
Anthony Reis <anthonyreis1503@gmail.com>
2026-03-19 21:39:05 -0300

    Projeto pronto

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ watts = int(input("Potência do aparelho em watts: "))
 horasDia = int(input("Horas de consumo diário: "))
 
 consumoMensal = (watts * horasDia * 30) / 1000
-print(f"a")
-print(f"Consumo mensal estimado de {consumoMensal} ")
+print(f"Aparelho: {aparelho}")
+print(f"Consumo estimado: {consumoMensal:.2f} kWh/mês")
 
+resposta = input("Você gostaria saber o preço por consumo? (s/n): ")
 
-
+if resposta == "s":
+    print("Calculando o preço...")
+    precoDoAparelho = (consumoMensal * 0.75)
+    print(f"O valor estimado para o consumo mensal do aparelho será de: R${precoDoAparelho}")
+else:
+    print("Ok, encerrando.")
