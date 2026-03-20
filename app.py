4924171eb3f0be0e96ee5cb22337cb41ba3fab26
Anthony Reis <anthonyreis1503@gmail.com>
2026-03-19 21:04:11 -0300

    segundo commit de teste

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ watts = int(input("Potência do aparelho em watts: "))
 horasDia = int(input("Horas de consumo diário: "))
 
 consumoMensal = (watts * horasDia * 30) / 1000
-print(f"")
+print(f"a")
 print(f"Consumo mensal estimado de {consumoMensal} ")
 
 
